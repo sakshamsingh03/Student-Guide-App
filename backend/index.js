d2f1c7315d370a254bd7e3d35b08061645b1aa77
Anurag Tripathi <113921508+anurag24-26@users.noreply.github.com>
2025-07-27 15:12:21 +0530

    Setup basic Express server with root route

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express=require('express');
+const app=express();
+const port=5000;
+
+
+
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send("Hello Student Guide backend is running");
+})
+
+
+app.listen(port,()=>{
+    console.log(`Server is buring coal at port ${port}`);
+})
